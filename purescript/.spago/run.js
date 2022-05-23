4457399e3f145c722724bcc4790b161970ce2a82
TineoC <christophertineo02@gmail.com>
2022-05-23 19:23:38 -0400

    Throw error un purescript

---
+++ purescript/.spago/run.js
@@ -0,0 +1,3 @@
+import { main } from 'file://C:/Users/chris/OneDrive/Documents/code/ids347/purescript/output/Main/index.js'
+
+main()
--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -1,6 +1,30 @@
 module Main where
 
-import Prelude
+-- import Prelude
+-- import Effect (Effect)
+-- import Effect.Console (log)
 
-addme :: Int -> Int -> Int
-addme a b = a + b
+-- 1. Add two numbers
+
+-- addme :: Int -> Int -> Int
+-- addme a b = a + b
+
+
+-- 2. Print string
+
+-- sayName :: String -> String
+
+-- sayName nombre = "Hola, " <> nombre <> "!"
+
+
+-- 3. Print Error message
+
+
+-- import unsafePerformEffect
+
+-- unsafePerformEffect :: forall a. String -> a
+-- error = unsafePerformEffect <<< throw
+
+-- 4. Enviroment var
+
+main :: unsafeThrow :: forall a. String -> a
