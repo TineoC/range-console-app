cb9f56db4c6b98c0a6d252c0faf42ecc294b9333
TineoC <christophertineo02@gmail.com>
2022-05-23 19:31:45 -0400

    final commit

--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -27,4 +27,60 @@ module Main where
 
 -- 4. Enviroment var
 
-main :: unsafeThrow :: forall a. String -> a
+-- main :: unsafeThrow :: forall a. String -> a
+
+-- 5. File read
+
+-- import Prelude
+-- import Effect (Effect)
+-- import Effect.Console (log)
+
+-- import Node.Encoding (Encoding(..))
+-- import Node.FS.Sync (readTextFile)
+
+-- main :: Effect Unit
+-- main = do
+--   log =<< readTextFile UTF8 ".env"
+
+  -- 6. Network I/O
+
+-- import Control.Monad.Maybe.Trans (lift, MaybeT(MaybeT), runMaybeT)
+-- import Data.Maybe (fromJust)
+-- import Effect (Effect)
+-- import Effect.Aff (runAff)
+-- import Effect.Class (liftEffect)
+-- import Effect.Console (log)
+-- import Partial.Unsafe (unsafePartial)
+-- import Prelude
+-- import Unsafe.Coerce (unsafeCoerce)
+-- import Web.DOM.Document (toNonElementParentNode)
+-- import Web.DOM.Element (toEventTarget)
+-- import Web.DOM.NonElementParentNode (getElementById)
+-- import Web.Event.EventTarget (eventListener, addEventListener)
+-- import Web.File.File (File, toBlob)
+-- import Web.File.FileList (item)
+-- import Web.File.FileReader.Aff (readAsArrayBuffer, readAsText)
+-- import Web.HTML (window)
+-- import Web.HTML.Event.EventTypes (change)
+-- import Web.HTML.HTMLDocument (toDocument, body)
+-- import Web.HTML.HTMLInputElement (HTMLInputElement, files)
+-- import Web.HTML.Window (document)
+
+-- main :: Effect Unit
+-- main = unsafePartial $ do
+--   d <- document =<< window
+--   b <- fromJust <$> body d
+--   fileInput <- fromJust <$> getElementById "fileInput" (toNonElementParentNode $ toDocument d)
+--   let inputElt = unsafeCoerce fileInput :: HTMLInputElement
+--   let reader :: File -> Effect Unit
+--       reader f = void $ runAff (\_ -> pure unit) do
+--         res <- readAsText (toBlob f)
+--         res2 <- readAsArrayBuffer (toBlob f)
+--         liftEffect $ log res
+
+--   handler <- eventListener \_ -> void $ runMaybeT do
+--     fs <- MaybeT $ files inputElt
+--     file <- MaybeT $ pure $ item 0 fs
+--     lift $ reader file
+
+--   addEventListener change handler false (toEventTarget $ fileInput)
